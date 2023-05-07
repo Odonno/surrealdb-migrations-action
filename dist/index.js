ddfb25e7d4161f25119b3068fe1dbc9ff6b2b940
Odonno <bottiau.david@laposte.net>
2023-05-07 20:05:05 +0200

    🔧 change prettier configuration

--- dist/index.js
+++ dist/index.js
@@ -44,12 +44,12 @@ const wait_1 = __nccwpck_require__(468);
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const ms = core.getInput('milliseconds');
+            const ms = core.getInput("milliseconds");
             core.debug(`Waiting ${ms} milliseconds ...`); // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
             core.debug(new Date().toTimeString());
             yield (0, wait_1.wait)(parseInt(ms, 10));
             core.debug(new Date().toTimeString());
-            core.setOutput('time', new Date().toTimeString());
+            core.setOutput("time", new Date().toTimeString());
         }
         catch (error) {
             if (error instanceof Error)
@@ -80,11 +80,11 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.wait = void 0;
 function wait(milliseconds) {
     return __awaiter(this, void 0, void 0, function* () {
-        return new Promise(resolve => {
+        return new Promise((resolve) => {
             if (isNaN(milliseconds)) {
-                throw new Error('milliseconds not a number');
+                throw new Error("milliseconds not a number");
             }
-            setTimeout(() => resolve('done!'), milliseconds);
+            setTimeout(() => resolve("done!"), milliseconds);
         });
     });
 }
