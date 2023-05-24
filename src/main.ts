e061e6dec4483be284bb5016385a33d2380b296a
Odonno <bottiau.david@laposte.net>
2023-05-24 12:42:18 +0200

    ♻️ split functions in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,83 +2,13 @@ import * as core from "@actions/core";
 import * as toolCache from "@actions/tool-cache";
 import * as exec from "@actions/exec";
 
-import getActionInputs from "./args";
-import resolveConfig from "./releases";
+import getActionInputs, { ActionInputs } from "./args";
+import getReleaseConfig from "./releases";
 import { retrieveMigrationDefinitionsPath } from "./config";
 import { isRepositoryDirty } from "./git";
 import { isFeatureAvailable } from "./features";
 
-async function run(): Promise<void> {
-  const inputs = getActionInputs();
-  const { downloadUrl, releaseVersion } = await resolveConfig(inputs);
-
-  core.info(
-    `[surrealdb-migrations] downloading surrealdb-migrations from ${downloadUrl}`
-  );
-  const surrealdbMigrationsTarballPath = await toolCache.downloadTool(
-    downloadUrl
-  );
-  const surrealdbMigrationsBinPath = await toolCache.extractTar(
-    surrealdbMigrationsTarballPath
-  );
-
-  core.addPath(surrealdbMigrationsBinPath);
-
-  const additionalArgs: string[] = [];
-
-  if (inputs.address) {
-    additionalArgs.push("--address", inputs.address);
-  }
-  if (inputs.url) {
-    additionalArgs.push("--url", inputs.url);
-  }
-  if (inputs.ns) {
-    additionalArgs.push("--ns", inputs.ns);
-  }
-  if (inputs.db) {
-    additionalArgs.push("--db", inputs.db);
-  }
-  if (inputs.username) {
-    additionalArgs.push("--username", inputs.username);
-  }
-  if (inputs.password) {
-    additionalArgs.push("--password", inputs.password);
-  }
-
-  const canApplyDryRun = isFeatureAvailable("apply --dry-run", releaseVersion);
-  const canApplyValidateVersionOrder = isFeatureAvailable(
-    "apply --validate-version-order",
-    releaseVersion
-  );
-
-  if (canApplyDryRun && canApplyValidateVersionOrder) {
-    core.info(`[surrealdb-migrations] checking is everything is right`);
-
-    const applyDryRunArgs = [
-      "apply",
-      "--dry-run",
-      "--validate-version-order",
-    ].concat(additionalArgs);
-    await exec.exec("surrealdb-migrations", applyDryRunArgs);
-
-    if (!inputs.skipUntrackedFiles) {
-      const definitionsFolderPath = retrieveMigrationDefinitionsPath();
-
-      if (await isRepositoryDirty(definitionsFolderPath)) {
-        core.error(
-          `[surrealdb-migrations] please commit definitions files before applying migrations`
-        );
-        throw new Error("Git repository is dirty");
-      }
-    }
-  }
-
-  const args = ["apply"].concat(additionalArgs);
-
-  core.info(`[surrealdb-migrations] applying migrations`);
-
-  await exec.exec("surrealdb-migrations", args);
-}
+main();
 
 async function main(): Promise<void> {
   try {
@@ -92,4 +22,99 @@ async function main(): Promise<void> {
   }
 }
 
-main();
+async function run(): Promise<void> {
+  const inputs = getActionInputs();
+  const { downloadUrl, releaseVersion } = await getReleaseConfig(inputs);
+
+  await installCli(downloadUrl);
+
+  const additionalApplyArgs: string[] = getAdditionalApplyArgs(inputs);
+
+  const canApplyDryRun = isFeatureAvailable("apply --dry-run", releaseVersion);
+  const canApplyValidateVersionOrder = isFeatureAvailable(
+    "apply --validate-version-order",
+    releaseVersion
+  );
+
+  if (canApplyDryRun && canApplyValidateVersionOrder) {
+    await checkApplyInDryRun(additionalApplyArgs, inputs);
+  }
+
+  await applyAllMigrations(additionalApplyArgs);
+}
+
+async function installCli(downloadUrl: string): Promise<void> {
+  core.info(
+    `[surrealdb-migrations] downloading surrealdb-migrations from ${downloadUrl}`
+  );
+
+  const surrealdbMigrationsTarballPath = await toolCache.downloadTool(
+    downloadUrl
+  );
+  const surrealdbMigrationsBinPath = await toolCache.extractTar(
+    surrealdbMigrationsTarballPath
+  );
+
+  core.addPath(surrealdbMigrationsBinPath);
+}
+
+function getAdditionalApplyArgs(inputs: ActionInputs): string[] {
+  const additionalApplyArgs: string[] = [];
+
+  if (inputs.address) {
+    additionalApplyArgs.push("--address", inputs.address);
+  }
+  if (inputs.url) {
+    additionalApplyArgs.push("--url", inputs.url);
+  }
+  if (inputs.ns) {
+    additionalApplyArgs.push("--ns", inputs.ns);
+  }
+  if (inputs.db) {
+    additionalApplyArgs.push("--db", inputs.db);
+  }
+  if (inputs.username) {
+    additionalApplyArgs.push("--username", inputs.username);
+  }
+  if (inputs.password) {
+    additionalApplyArgs.push("--password", inputs.password);
+  }
+
+  return additionalApplyArgs;
+}
+
+async function checkApplyInDryRun(
+  additionalApplyArgs: string[],
+  inputs: ActionInputs
+): Promise<void> {
+  core.info(`[surrealdb-migrations] checking if everything is right`);
+
+  const applyDryRunArgs = [
+    "apply",
+    "--dry-run",
+    "--validate-version-order",
+  ].concat(additionalApplyArgs);
+
+  await exec.exec("surrealdb-migrations", applyDryRunArgs);
+
+  if (!inputs.skipUntrackedFiles) {
+    const definitionsFolderPath = retrieveMigrationDefinitionsPath();
+
+    if (await isRepositoryDirty(definitionsFolderPath)) {
+      core.error(
+        `[surrealdb-migrations] please commit definitions files before applying migrations`
+      );
+      throw new Error("Git repository is dirty");
+    }
+  }
+}
+
+async function applyAllMigrations(
+  additionalApplyArgs: string[]
+): Promise<void> {
+  const args = ["apply"].concat(additionalApplyArgs);
+
+  core.info(`[surrealdb-migrations] applying migrations`);
+
+  await exec.exec("surrealdb-migrations", args);
+}
